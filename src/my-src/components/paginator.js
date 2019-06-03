53237a40ecaa5b80add817b45cc4e964b3974149
s.garyaga@lucky-labs.com <you@example.com>
2019-06-03 19:01:13 +0300

    fix

--- src/my-src/components/paginator.js
+++ src/my-src/components/paginator.js
@@ -1,31 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
 
-function createPages(countPage) {
-    const result = [];
-    for (let i = 0; i < countPage; i++) {
-        result.push(i + 1);
+function* generatePage(countPage) {
+    for (let i = 1; i <= countPage; i++) {
+        yield i;
     }
-
-    return result;
 }
 
 const Paginator = ({ countPage, activePage, dispatch }) => {
-    const nextPage = activePage === 1 ? 1 : activePage + 1;
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination">
-                <li className="page-item"><a className="page-link" href="javascript:void(0)" onClick={
+                <li className="page-item"><button className="page-link" onClick={
                     () => dispatch({ type: "ACTIVE_PAGE", activePage: activePage === 1 ? 1 : activePage - 1 })
-                }>Previous</a></li>
+                }>Previous</button></li>
                 {
-                    createPages(countPage).map((page, i) => <li className={"page-item" + (page == activePage ? " active" : "")} key={i}>
-                        <a className="page-link" href="javascript:void(0)" onClick={() => dispatch({ type: "ACTIVE_PAGE", activePage: page })}>{page}</a>
+                    [...generatePage(countPage)].map((page, i) => <li className={"page-item" + (page === activePage ? " active" : "")} key={i}>
+                        <button className="page-link" onClick={() => dispatch({ type: "ACTIVE_PAGE", activePage: page })}>{page}</button>
                     </li>)
                 }
-                <li className="page-item"><a className="page-link" href="javascript:void(0)" onClick={
+                <li className="page-item"><button className="page-link" onClick={
                     () => dispatch({ type: "ACTIVE_PAGE", activePage: activePage === countPage ? countPage : activePage + 1 })
-                }>Next</a></li>
+                }>Next</button></li>
             </ul>
         </nav>
     )
